d28da0ed562d35ea08a63aa8a5381c706ab86a94
manlikeayei <eyongeno@outlook.com>
2025-02-05 13:37:09 +0100

    minor changes

--- src/main/java/com/example/ayei/demo_delivery/DemoDeliveryApplication.java
+++ src/main/java/com/example/ayei/demo_delivery/DemoDeliveryApplication.java
@@ -3,8 +3,9 @@ package com.example.ayei.demo_delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
-
+@EnableWebMvc
 @ComponentScan(basePackages = "com.example.ayei.demo_delivery.services.ItemService")
 public class DemoDeliveryApplication {
 
